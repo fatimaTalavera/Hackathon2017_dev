6758877e74921d0e30f296245a7aaa0d3503e6bc
PanyPy <diazpany@gmail.com>
2017-09-15 09:27:04 -0400

    eventos en la barra de progreso

--- app/assets/javascripts/anychart/progress-chart-data.js
+++ app/assets/javascripts/anychart/progress-chart-data.js
@@ -143,7 +143,7 @@ function loadYearsSelect(){
 function loadInstitutesSelect(){
     var select = document.getElementById('instituteSelectProgress');
     var option = document.createElement("option");
-    option.value = '';
+    option.value = 'default';
     option.text = 'Seleccione una Institución';
     select.appendChild(option);
     for (var i = 0; i < inst_count; i++) {
