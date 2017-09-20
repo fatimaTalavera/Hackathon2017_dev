b8889ca7a7ea9b2d3be669be9af37b57eb96fe07
PanyPy <diazpany@gmail.com>
2017-09-20 02:43:35 -0400

    actualizo los mapas

--- app/assets/javascripts/anychart/anychart-map-data.js
+++ app/assets/javascripts/anychart/anychart-map-data.js
@@ -81,8 +81,74 @@ function updateHeatMap(e) {
         url: "search/search",
         data: {"year" : $('#yearSelectHeatMap').val(), "month" : $('#monthSelectHeatMap').val(), "q" : "department_heat_map" }
     })
-        .done(function( msg ) {
-            Window.currentDataMap = msg;
+        .done(function( values ) {
+            Window.currentDataMap = values;
+
+            Window.oldGeoJSONgroup = Window.geoJSONgroup;
+            setTimeout("Window.map.removeLayer(Window.oldGeoJSONgroup)", 4000);
+
+
+
+            $.getJSON('/paraguay.json', function (geoJSONdata) {
+
+                        //console.log('valuies');
+                        //console.log(geoJSONdata.features);
+                        var min = values[1][5]*100/values[1][2];
+                        var max = min;
+
+                        $.each( geoJSONdata.features, function( key, val ) {
+                            var dpto = val.properties.dpto;
+                            var valByDpto = values[parseInt(dpto)];
+                            var currentValue = valByDpto[5]*100/valByDpto[2];
+                            if(currentValue < min){
+                                min = currentValue;
+                            }
+                            if(currentValue > max){
+                                max = currentValue;
+                            }
+                        });
+                        // console.log("max " + max + "-min: " + min);
+                        $.each( geoJSONdata.features, function( key, val ) {
+                            //  console.log('here');
+                            //   console.log(val);
+                            //   console.log(values);
+                            var dpto = val.properties.dpto;
+                            var valByDpto = values[parseInt(dpto)];
+                            val.properties['data'] = valByDpto;
+                            var currentValue = valByDpto[5]*100/valByDpto[2];
+                            val.properties['value'] = currentValue;
+                            val.properties['min'] = min;
+                            val.properties['max'] = max;
+                        });
+
+                        Window.geoJSONgroup = L.geoJSON(geoJSONdata, {style: style}).addTo(Window.map);
+
+                        Window.geoJSONgroup.eachLayer(function (layer) {
+                            if (layer.feature.properties.name === "Paraguay") {
+                                Window.map.fitBounds(layer.getBounds());
+                            }
+                        });
+                        Window.geoJSONgroup.on('click', function(e) {
+                            console.log('check');
+                            console.log(Window.map);
+                            console.log(Window.currentDataMap);
+                            var currentData = Window.currentDataMap[parseInt(e.layer.feature.properties.dpto)];
+                            $('#departmentName').text(e.layer.feature.properties.dpto_desc);
+                            $('#departmentDetail').show();
+                            var instituteData = ['presupuesto_aprobado', 'presupuesto_vigente', 'monto_planificado', 'monto_ejecutado', 'monto_transferido', 'monto_abonado'];
+                            var counter = 1;
+                            instituteData.forEach(function (element) {
+                                var elementTitle = $("#departmentDetail\\["+element+"\\]").html();
+                                var newElementTitle = elementTitle.match(/<strong>(.*)strong>/)
+                                //console.log(newElementTitle[0]);
+                                $("#departmentDetail\\["+element+"\\]").html(newElementTitle[0]+' '+format_currency(currentData[counter]));
+                                $("#departmentDetail\\["+element+"\\]").show();
+                                counter++;
+                            });
+
+                        });
+                    });
+
 
         });
 }
