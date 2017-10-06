815c106ce3f96d9e3e2d01d5e0834c8a2d0198a2
fatimaTalavera <fa.talavera95@gmail.com>
2017-10-06 00:30:41 -0300

    agregar estilos para rating

--- app/assets/javascripts/rate.js
+++ app/assets/javascripts/rate.js
@@ -5,10 +5,29 @@
  #Jerson Paniagua diazpany@gmail.com
  #
  */
-
+// nominal value by  rate
+var nominalValue = ['Nada satisfecho', 'Poco satisfecho', 'Satisfecho', 'Muy satisfecho', 'Sumamente satisfecho'];
 $(document).ready(function() {
-    $("#map-rate").rateYo({
-        rating: 3.6
-    });
+    initRate("#map-rate", 3.6);
+    initRate("#progress-rate", 3.6);
 });
 
+// Init default rate
+var initRate = function (identificator, initRating, changeCallback) {
+    $(identificator).rateYo({
+        rating: initRating,
+        starWidth: "25px",
+        fullStar: true,
+        onInit: function (rating, rateYoInstance) {
+            $(this).prev().find('.calification').text(rating.toFixed(1));
+            $(this).next().find('.nominal-value').text(nominalValue[rating.toFixed(0) - 1]);
+        },
+        onSet: function (rating, rateYoInstance) {
+            $(this).next().find('.nominal-value').text(nominalValue[rating.toFixed(0) - 1]);
+            if(typeof changeCallback !== 'undefined') changeCallback(rating, this);
+        },
+        onChange: function (rating, rateYoInstance) {
+            $(this).next().find('.nominal-value').text(nominalValue[rating.toFixed(0) - 1]);
+        }
+    });
+};
